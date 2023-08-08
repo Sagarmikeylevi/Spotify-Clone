e8774e7d87bd76e4e5ab9860f63a902e9778221a
unknown <sagarmikeylevi@gmail.com>
2023-08-08 20:10:34 +0530

    complete the login UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import React from "react";
+import Login from "./components/Login";
 
 function App() {
-
   return (
     <>
-      <h1 className="text-red-600">Hello</h1>
+      <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,20 @@
+const Login = () => {
+  return (
+    <div className="flex flex-col h-screen justify-evenly items-center bg-[#3fca70]">
+      <img
+        className="w-[80%] md:w-[75%] lg:w-[60%]"
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
+        alt="spotify-logo"
+      />
+
+      <a
+        href="#"
+        className="uppercase bg-black text-white text-sm py-4 px-8 lg:text-base lg:py-5 lg:px-10 rounded-full font-medium tracking-wider transition-all duration-200 hover:translate-y-[-3px] hover:shadow-2xl  active:translate-y-[-1px] active:shadow-xl"
+      >
+        Connect to spotify
+      </a>
+    </div>
+  );
+};
+
+export default Login;
