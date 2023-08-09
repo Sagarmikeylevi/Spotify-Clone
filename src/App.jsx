2baf9d1b2586398cf7de10a11217c7842d41e554
unknown <sagarmikeylevi@gmail.com>
2023-08-09 19:48:08 +0530

    completed the login part and get the access token

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./components/Login";
+import { getTokenFromUrl } from "./Spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      console.log(_token);
+    }
+
+    
+  }, []);
   return (
     <>
-      <Login />
+      {token && <h1>I am logged in...</h1>}
+
+      {!token && <Login />}
     </>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,16 @@
+import { accessUrl } from "../Spotify";
+
 const Login = () => {
   return (
     <div className="flex flex-col h-screen justify-evenly items-center bg-[#3fca70]">
       <img
         className="w-[80%] md:w-[75%] lg:w-[60%]"
-        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png"
         alt="spotify-logo"
       />
 
       <a
-        href="#"
+        href={accessUrl}
         className="uppercase bg-black text-white text-sm py-4 px-8 lg:text-base lg:py-5 lg:px-10 rounded-full font-medium tracking-wider transition-all duration-200 hover:translate-y-[-3px] hover:shadow-2xl  active:translate-y-[-1px] active:shadow-xl"
       >
         Connect to spotify
